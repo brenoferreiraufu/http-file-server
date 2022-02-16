1348dc193d9089c30ff7d0c3b50054f70d5b2e57
brenoferreiraufu <breno.ferreira@ufu.br>
2022-02-15 22:02:50 -0300

    WIP

--- http-file-server.c
+++ http-file-server.c
@@ -1,12 +1,174 @@
-#include <sys/socket.h>
-#include <pthread.h>
-#include <stdlib.h>
+#include <sys/socket.h> // socket API
+#include <pthread.h>    // thread API
+#include <stdlib.h>     // EXIT_FAILURE
+#include <sys/ioctl.h>  // ioctl()
+#include <netinet/in.h> // struct sockaddr_in
+#include <string.h>     // memset()
+#include <stdio.h>      // printf()
+#include <unistd.h>     // close()
 
 #define ERROR -1
+#define SUCCESS 0
+#define TRUE 1
+#define FALSE 0
 
-int main(int argc, char const *argv[]) {
-    int server_sock, on = 1;
-    int status;
+#define PORT 80
+#define LISTEN_BACKLOG 4096
+#define REQUEST_BUFFER_SIZE 8192
+#define FILENAME_SIZE 256
+#define HTTP_STATUS_404 "HTTP/1.0 404 Not Found\r\n \
+                        Connection: Closed\r\n\n"
+#define HTTP_STATUS_200 "HTTP/1.0 200 OK\r\n \
+                        Connection: Closed\r\n\n"
+
+int server_sock, on = 1;
+int status, addr_size;
+struct sockaddr_in server_addr;
+
+void guard(char *fail, char *success)
+{
+    if (status == ERROR)
+    {
+        perror(fail);
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf(success);
+}
+
+void *connection_handler(void *arg)
+{
+    char buffer[REQUEST_BUFFER_SIZE];
+    char filename[FILENAME_SIZE] = {'\0'};
+    int client_sock = (long)arg;
+    int data_length, bytes_written;
+    int fsize, bytes_read;
+    FILE *file;
+
+    /******************************************************/
+    /* Recebe os dados da requisição e guarda no buffer.  */
+    /******************************************************/
+
+    data_length = recv(client_sock, buffer, sizeof(buffer), 0);
+
+    printf("[*] Client %d: Trying to collect request.\n", client_sock);
+    if (data_length == ERROR)
+    {
+        perror("Failed to recieve data.\n");
+        close(client_sock);
+        pthread_exit(NULL);
+    }
+
+    if (data_length == 0)
+    {
+        printf("[+] Client %d: Closed connection.\n", client_sock);
+        pthread_exit(NULL);
+    }
+
+    printf("[+] Client %d: Successful received data.\n", client_sock);
+
+    /******************************************************/
+    /* Lê a requisição para pegar o nome do arquivo.      */
+    /******************************************************/
+
+    int start = strcspn(buffer, "/") + 1;
+    int end = strcspn(buffer, "\r") - 14;
+    strncpy(filename, buffer + start, end);
+    
+    if (strlen(filename) == 0) 
+        strcat(filename, "index.html");
+
+    /******************************************************/
+    /* Tenta abrir o arquivo para ler os dados.           */
+    /******************************************************/
+
+    file = fopen(filename, "rb");
+
+    printf("[*] Client %d: Trying to open file.\n", client_sock);
+    if (file == NULL)
+    {
+        perror("[-] File not found.\n");
+        bytes_written = send(client_sock, HTTP_STATUS_404, strlen(HTTP_STATUS_404), 0);
+
+        printf("[*] Client %d: Sending not found response.\n", client_sock);
+        if (bytes_written == ERROR)
+        {
+            perror("[-] Failed to send data.\n");
+            close(client_sock);
+            pthread_exit(NULL);
+        }
+
+        close(client_sock);
+        pthread_exit(NULL);
+    }
+
+    printf("[+] Client %d: Found file %s\n", client_sock, filename);
+
+    /******************************************************/
+    /* Envia os dados do arquivo para o cliente.          */
+    /******************************************************/
+
+    fseek(file, 0, SEEK_END);
+    fsize = ftell(file);
+    rewind(file);
+
+    printf("[+] Client %d: File contains %ld bytes!\n", client_sock, fsize);
+
+    bytes_written = send(client_sock, HTTP_STATUS_200, strlen(HTTP_STATUS_200), 0);
+
+    printf("[*] Client %d: Sending ok response headers.\n", client_sock);
+    if (bytes_written == ERROR)
+    {
+        perror("[-] Failed to send data.\n");
+        close(client_sock);
+        fclose(file);
+        pthread_exit(NULL);
+    }
+
+    printf("[*] Client %d: Sending file...\n", client_sock);
+    while (TRUE)
+    {
+        bytes_read = fread(buffer, sizeof(char), sizeof(buffer), file);
+        if (bytes_read == 0)
+            break;
+
+        if (bytes_read < 0)
+        {
+            perror("[-] ERROR reading from file\n");
+            fclose(file);
+            close(client_sock);
+            pthread_exit(NULL);
+        }
+
+        while (bytes_read > 0)
+        {
+            bytes_written = send(client_sock, buffer, bytes_read, 0);
+
+            if (bytes_written == ERROR)
+            {
+                perror("[-] Failed to send data.\n");
+                close(client_sock);
+                fclose(file);
+                pthread_exit(NULL);
+            }
+
+            bytes_read -= bytes_written;
+        }
+    }
+
+    printf("[+] Client %d: Successful sent data.\n", client_sock);
+
+    fclose(file);
+    close(client_sock);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char const *argv[])
+{
+    int server_on = TRUE;
+    long client_sock;
+    pthread_t thread_id;
 
     /******************************************************/
     /* Cria um socket TCP (SOCK_STREAM).                  */
@@ -14,20 +176,106 @@ int main(int argc, char const *argv[]) {
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server_sock == ERROR) {
+    if (server_sock == ERROR)
+    {
         perror("[-] Failed to create a socket.\n");
         exit(EXIT_FAILURE);
     }
 
     printf("[+] TCP socket created successfully.\n");
 
-
     /******************************************************/
     /* Permite que o socket descriptor seja reutilizado.  */
     /******************************************************/
 
-    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on))
+    status = setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on));
+    guard("[-] Failed to set socket options.\n",
+          "[+] Socket descriptor allowed to be reused.\n");
 
+    /******************************************************/
+    /* Configurando o socket com I/O não bloqueante.      */
+    /******************************************************/
+
+    // status = ioctl(server_sock, FIONBIO, (char *)&on);
+    // guard("[-] Failed to set socket to be nonblocking.\n",
+    //     "[+] Socket descriptor set to be nonblocking.\n");
+
+    /******************************************************/
+    /* Inicializando estrutura do socket address.         */
+    /******************************************************/
+
+    addr_size = sizeof(server_addr);
+    memset(&server_addr, 0, addr_size);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    /******************************************************/
+    /* Bind do socket com o endereço em server_addr.      */
+    /******************************************************/
+
+    status = bind(server_sock, (struct sockaddr *)&server_addr, addr_size);
+    guard("[-] Failed to bind the socket to the address and port number.\n",
+          "[+] Success to bind the socket to the address and port number.\n");
+
+    /******************************************************/
+    /* Passa a escutar conexões com o socket.             */
+    /******************************************************/
+
+    status = listen(server_sock, LISTEN_BACKLOG);
+    guard("[-] Failed to listen requests.\n", "[+] Listening...\n");
+
+    /******************************************************/
+    /* Loop infinito enquanto permite conexões.           */
+    /******************************************************/
+
+    do
+    {
+
+        /******************************************************/
+        /* Aguarda até que uma nova conexão seja aceita.      */
+        /******************************************************/
+
+        client_sock = accept(server_sock, NULL, NULL);
+
+        if (client_sock == ERROR)
+        {
+            perror("[-] Failed to accept connection request.\n");
+            close(server_sock);
+            exit(EXIT_FAILURE);
+        }
+
+        /******************************************************/
+        /* Cria uma nova thread para lidar com a requisição.  */
+        /******************************************************/
+
+        status = pthread_create(&thread_id, NULL, connection_handler, (void *)client_sock);
+
+        if (status != SUCCESS)
+        {
+            perror("[-] Failed to create a new pthread.\n");
+            close(server_sock);
+            close(client_sock);
+            exit(EXIT_FAILURE);
+        }
+
+        /******************************************************/
+        /* Para desalocar a memória da thread quando rotina   */
+        /* finalizar usa a função pthread_detach.             */
+        /******************************************************/
+
+        status = pthread_detach(thread_id);
+
+        if (status != SUCCESS)
+        {
+            perror("[-] Failed to detach thread.\n");
+            close(server_sock);
+            close(client_sock);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("[+] New detached thread created for connection %d.\n", client_sock);
+    } while (TRUE);
 
     return 0;
 }
