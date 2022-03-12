a804262ffa7a926b633825acdfbc4ac3b22ad46a
brenoferreiraufu <breno.ferreira@ufu.br>
2022-03-12 20:12:13 -0300

    Delete useless files.

--- comp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc http-file-server.c -lpthread
-./a.out
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-i=4096
-ab -n $i -c $i -s 60 http://127.0.0.1:80/arq2.dat > "./log/test_60_2_$i.txt"
-ab -n $i -c $i -s 300 http://127.0.0.1:80/arq2.dat > "./log/test_300_2_$i.txt"
